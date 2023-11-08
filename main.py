80c99e9d7bfe40b3ebd7b31f11928c480657570b
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-11-07 18:42:52 -1000

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class AdalineGD:
+    '''
+    ADAptive LInear classifier
+
+    Credit to "Machine Learning with PyTorch and SciKit-Learn"
+    '''
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+
+        self.b_ = np.float(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (errors ** 2).mean()
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
