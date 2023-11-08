f1323942895738733a250a91b6c0ad90c6776aa6
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-11-07 19:14:34 -1000

    added sgd algo

--- main.py
+++ main.py
@@ -38,3 +38,74 @@ class AdalineGD:
 
     def predict(self, X):
         return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+
+class AdalineSGD:
+    """
+    ADAptive LInear classifier with stochastic gradient descent rather than batch gradient descent.
+    Stochastic gradient descent updates per example, rather than summing all examples, which is computationally costly.
+
+    Credit to "Machine Learning with PyTorch and SciKit-Learn"
+    """
+
+    def __init__(self, eta=0.01, n_iter=50, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        self._initialize_weights(X.shape[1])
+        self.losses = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            losses = []
+            for xi, target in zip(X, y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.losses_.append(avg_loss)
+        return self
+
+    def partial_fit(self, X, y):
+        """Fit data without reinitializing weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=m)
+        self.b_ = np.float(0.)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """Apply learning rule to update weights"""
+        output = self.activation(self.net_input(xi))
+        error = (target -output)
+        self.w_ += self.eta * 2.0 * xi * (error)
+        self.b_ += self.eta * 2.0 * error
+        loss = error**2
+        return loss
+
+    def net_input(self, X):
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        return X
+
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+
